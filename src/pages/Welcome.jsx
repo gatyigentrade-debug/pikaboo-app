d3fb8bb548540eca8a7117b93cd6a48622d5b045
Nathi Bukelwa <gatyigentrade@gmail.com>
2026-08-18 13:24:29 +0000

    Visual edits

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -30,7 +30,7 @@ export default function Welcome() {
           background: "linear-gradient(180deg, rgba(5,3,0,0) 0%, rgba(5,3,0,0) 30%, rgba(5,3,0,0.85) 62%, #050300 100%)"
         }} />
 
-      <div className="relative z-10 w-full max-w-sm flex flex-col items-center pr-4 pl-2 pb-2 pt-16">
+      <div className="relative z-10 w-full max-w-sm flex flex-col items-center pl-2 pt-5 pr-6 pb-8">
         {/* Wordmark */}
         
 
